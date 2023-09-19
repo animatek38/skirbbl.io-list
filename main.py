07bd8b8ab14e59096ce21b917b632b92758b41c8
animatek <51943119+animatek38@users.noreply.github.com>
2023-09-19 20:19:49 +0200

    second commit

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 from tkinter import *
 from tkinter import ttk
+import os
+
+
+def createList(listName: str, arg: list):
+    with open(r'./list/' + listName, 'w') as fp:
+    for i int arg:
+        fp.write(i)
+    return 1;
+
+def loadList(listName: str):
+    pass
+
+
+def refreshLists():
+    pass
+
+
+
