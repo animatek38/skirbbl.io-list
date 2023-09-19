e1a7c59f03a7b183fb93972e75d8df09ee8f255e
TheBlueNeon <marius.poncon@gmail.com>
2023-09-19 19:25:56 +0100

    update

--- main/main.py
+++ main/main.py
@@ -5,9 +5,9 @@ import os
 
 def createList(listName: str, arg: list):
     with open(r'./list/' + listName, 'w') as fp:
-    for i int arg:
-        fp.write(i)
-    return 1;
+        for i in arg:
+            fp.write(i)
+        return 1
 
 def loadList(listName: str):
     pass
