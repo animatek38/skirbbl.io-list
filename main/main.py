c58a1d22710462228fe524cb0c4c20cd016f6224
TheBlueNeon <marius.poncon@gmail.com>
2023-09-19 20:01:36 +0100

    test

--- main/main.py
+++ main/main.py
@@ -2,9 +2,15 @@ from tkinter import *
 from tkinter import ttk
 import os
 
+root = Tk()
+frm = ttk.Frame(root, width=560, height=420)
+frm.grid()
+
+l = Label(root, text = "Fact of the Day")
+l.config(font =("Courier", 14))
 
 def createList(listName: str, arg: list):
-    with open(r'./list/' + listName, 'w') as fp:
+    with open(r'./list/list.txt' + listName, 'w') as fp:
         for i in arg:
             fp.write(i)
         return 1
@@ -12,9 +18,8 @@ def createList(listName: str, arg: list):
 def loadList(listName: str):
     pass
 
-
 def refreshLists():
     pass
 
-
+root.mainloop()
 
